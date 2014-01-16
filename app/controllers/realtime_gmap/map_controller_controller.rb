84a86933d38e6260f63856083edd4f2c68664108
Chris Locke <clocke@degica.com>
2014-01-16 16:23:23 +0900

    Move helper functions to application_helper.rb

--- app/controllers/realtime_gmap/map_controller_controller.rb
+++ app/controllers/realtime_gmap/map_controller_controller.rb
@@ -2,10 +2,17 @@ require_dependency "realtime_gmap/application_controller"
 
 module RealtimeGmap
   class MapControllerController < ApplicationController
+    
     def index
+      respond_to do |format|
+        format.html
+      end
     end
 
     def query
+      respond_to do |format|
+        format.json { render :json => nil }
+      end
     end
   end
 end
